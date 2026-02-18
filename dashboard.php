96664ea9ad1ee5ce1605b4825cbd130fafc7e2a7
MonicaR1108 <monicaravikumar1@gmail.com>
2026-02-18 15:56:56 +0530

    stylesadded

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,18 @@ if(!isset($_SESSION['user_id'])){
     header("Location: login.php");
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-<h2>Welcome <?php echo $_SESSION['name']; ?></h2>
-
-<a href="logout.php">Logout</a>
+<div class="dashboard-box">
+<h2 >Welcome <?php echo $_SESSION['name']; ?></h2>
+<p>You are successfully logged in.</p>
+<a href="logout.php" class="logout-btn">Logout</a>
+</div>
+</body>
+</html>
